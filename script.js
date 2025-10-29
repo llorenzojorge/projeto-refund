07ce882c6c4b7414fe52e904dd03419bae5aae9b
Lorenzo <lorenzojorge932@gmail.com>
2025-10-29 11:33:57 -0300

    Revert "add comentarios"
    This reverts commit 57cad6a40c25a21c8205269b7bdcda50a4415b5d.

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // Seleciona os elementos do formulário
 const amount = document.getElementById("amount")
 
-// Captura o evento de input para formatar o valor
+// Aceitando somentos números no valor da despesa
 amount.oninput = () => {
-  // Obtém o valor atual do input e remove os caracteres não númericos
   let value = amount.value.replace(/\D/g, "")
   
-  // Atualiza o valor do input para o usuário
   amount.value = value
 }
