57cad6a40c25a21c8205269b7bdcda50a4415b5d
Lorenzo <lorenzojorge932@gmail.com>
2025-10-29 11:32:40 -0300

    add comentarios

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Seleciona os elementos do formulário
 const amount = document.getElementById("amount")
 
-// Aceitando somentos números no valor da despesa
+// Captura o evento de input para formatar o valor
 amount.oninput = () => {
+  // Obtém o valor atual do input e remove os caracteres não númericos
   let value = amount.value.replace(/\D/g, "")
   
+  // Atualiza o valor do input para o usuário
   amount.value = value
 }
