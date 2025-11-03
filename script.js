70fd06ec92048cae25342f5768131a804a43db98
Lorenzo <lorenzojorge932@gmail.com>
2025-11-03 11:38:25 -0300

    calculando o valor total

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const amount = document.getElementById("amount")
 
 // Seleciona os elementos da lista
 const expenseList = document.querySelector("ul")
+const expensesTotal = document.querySelector("aside header h2")
 const expensesQuantity = document.querySelector("aside header p span")
 
 // Captura o evento de input para formatar o valor
@@ -123,9 +124,22 @@ function updateTotals() {
     for (let item = 0; item < items.length; item++) {
       const itemAmount = items[item].querySelector(".expense-amount")
 
-      console.log(itemAmount)
+      // Removendo caracteres não numéricos e substituindo a vírgula pelo ponto
+      let value = itemAmount.textContent.replace(/[^\d]/g, "").replace(",", ".")
+
+      // Converte o valor para float
+      value = parseFloat(value)
+
+      // Verifica se é um número válido
+      if(isNaN(value)) {
+        return alert("Não foi possível calcular o total. O valor não parece ser um número.")
+      }
+
+      // Incrementar o valor total
+      total += Number(value)
     } 
 
+    expensesTotal.textContent = total
   } catch (error) {
     console.log(error)
     alert("Não foi possível atualizar os totais")
